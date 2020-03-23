a146df0e1ac1655342d549461197000c98b4b668
V Pranay Bharadwaj <pranaybharadwajv@gmail.com>
2020-03-23 12:43:47 +0530

    Updated 2nd que solution with collections

--- Que2.py
+++ Que2.py
@@ -2,7 +2,7 @@ class College:
         Student1 = {'Name':'Pranay','Stream':'BTech','Year':'1','Sec':'101','Attendance':'A'}
         Student2 = {'Name':'Minal','Stream':'MBA','Year':'1','Sec':'101','Attendance':'A'}
         Student3 = {'Name':'Shinam','Stream':'BTech','Year':'1','Sec':'101','Attendance':'P'}
-        Student4 = {'Name':'shanay','Stream':'MBA','Year':'1','Sec':'101','Attendance':'P'}
+        Student4 = {'Name':'shanay','Stream':'MBA','Year':'1','Sec':'101','Attendance':''}
         Students =[Student1,Student2,Student3,Student4]
 
         def Student_Register(self):
@@ -18,6 +18,7 @@ class BTech:
             BTech_Register.append(x)
             
     def BTech_Student_Register(self):
+        
         return self.BTech_Register
 
 class MBA:
@@ -34,18 +35,20 @@ class MBA:
 
 
 class BTech_Year1:
-    temp =[]
+    
     BTech_Year1_Register=[]
     bt_y1 = BTech()
     Bt_Y1_Meth = bt_y1.BTech_Student_Register()
     
     for x in Bt_Y1_Meth:
         if x['Year'] == '1':
-            temp.append(x['Name'])
-            temp.append(x['Sec'])
-            temp.append(x['Attendance'])
-            BTech_Year1_Register.append(temp)
-            temp = []
+                name = x['Name']
+                sec = x['Sec']
+                attend = x['Attendance']
+                temp = {'Name':name,'Sec':sec,'Attendance':attend}
+                BTech_Year1_Register.append(temp)
+                #temp.clear()
+            
             
     def BTech_Student_Year1_Register(self):
         return self.BTech_Year1_Register
@@ -58,55 +61,49 @@ class MBA_Year1:
     
     for x in mb_Y1_Meth:
         if x['Year'] == '1':
-            temp.append(x['Name'])
-            temp.append(x['Sec'])
-            temp.append(x['Attendance'])
-            MBA_Year1_Register.append(temp)
-            temp = []
-            
+                name = x['Name']
+                sec = x['Sec']
+                attend = x['Attendance']
+                temp = {'Name':name,'Sec':sec,'Attendance':attend}
+                MBA_Year1_Register.append(temp)            
     def MBA_Student_Year1_Register(self):
         return self.MBA_Year1_Register
 
 class BTech_Year1_Sec101:
-    temp = []
     BTech_Year1_Sec101_Register=[]
     bt_y1_101 = BTech_Year1()
     BTech_Year1_Sec101_Meth = bt_y1_101.BTech_Student_Year1_Register()
     
-    for y in range(len(BTech_Year1_Sec101_Meth)):
-        if BTech_Year1_Sec101_Meth[y][1] == '101':
-                temp.append(BTech_Year1_Sec101_Meth[y][0])
-                temp.append(BTech_Year1_Sec101_Meth[y][2])
+    for x in BTech_Year1_Sec101_Meth:
+        if x['Sec'] == '101':
+                name = x['Name']
+                attend = x['Attendance']
+                temp = {'Name':name,'Attendance':attend}
                 BTech_Year1_Sec101_Register.append(temp)
-                temp = []
-
     def Absent_Students(self):
         Section_Attendance = self.BTech_Year1_Sec101_Register
         print("Absent Students of BTech 1st year 101 are :")
-        for y in range(len(Section_Attendance)):
-                if Section_Attendance[y][1] == 'A':
-                    print(Section_Attendance[y][0])
+        for y in Section_Attendance:
+                if y['Attendance'] == 'A':
+                    print(y['Name'])
 
 class MBA_Year1_Sec101:
-    temp = []
     MBA_Year1_Sec101_Register=[]
     mb_y1_101 = MBA_Year1()
     MBA_Year1_Sec101_Meth = mb_y1_101.MBA_Student_Year1_Register()
     
-    for y in range(len(MBA_Year1_Sec101_Meth)):
-        if MBA_Year1_Sec101_Meth[y][1] == '101':
-                temp.append(MBA_Year1_Sec101_Meth[y][0])
-                temp.append(MBA_Year1_Sec101_Meth[y][2])
+    for x in MBA_Year1_Sec101_Meth:
+        if x['Sec'] == '101':
+                name = x['Name']
+                attend = x['Attendance']
+                temp = {'Name':name,'Attendance':attend}
                 MBA_Year1_Sec101_Register.append(temp)
-                temp = []
-
-    #Polymorphism with same function name as in BTech_Year1_Sec101
     def Absent_Students(self):
         Section_Attendance = self.MBA_Year1_Sec101_Register
-        print("\nAbsent Students of MBA 1st year 101 are :")
-        for y in range(len(Section_Attendance)):
-                if Section_Attendance[y][1] == 'A':
-                    print(Section_Attendance[y][0])
+        print("\nAbsent Students of BTech 1st year 101 are :")
+        for y in Section_Attendance:
+                if y['Attendance'] == 'A':
+                    print(y['Name'])
 
 #Calling Absent_Students in BTech_Year1_Sec101, MBA_Year1_Sec101 classes using Student_Register object, in College Class                
 
